b2080b4b7ba4d74021684a8fa1d61432e4287377
AutisticCrumbs <ecrump@bsu.edu>
2023-10-01 00:19:59 -0400

    Add an area that plays a sound effect when walked over

--- gerald/Gerald.gd
+++ gerald/Gerald.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
+signal end_game
+
 func _physics_process(_delta):
 	if Input.is_action_pressed("up"):
-		position.y -= 3
+		position.y -= 5
 	if Input.is_action_pressed("down"):
-		position.y += 3
+		position.y += 5
 	if Input.is_action_pressed("left"):
-		position.x -= 3
+		position.x -= 5
 	if Input.is_action_pressed("right"):
-		position.x += 3
+		position.x += 5
 
 	move_and_slide()
---
+++ sound_triggers/end_area/end_area.gd
@@ -0,0 +1,11 @@
+extends Area2D
+
+func _on_body_entered(_body):
+	var audio_stream_player := AudioStreamPlayer.new()
+	audio_stream_player.stream = load("res://sound_triggers/sfx/end_beep.wav")
+	get_parent().add_child(audio_stream_player)
+	audio_stream_player.play()
+	audio_stream_player.finished.connect(func():
+		audio_stream_player.queue_free()
+		get_tree().change_scene_to_file("res://end_screen/end_screen.tscn")
+	)
