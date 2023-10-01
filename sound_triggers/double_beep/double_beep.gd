4cbf943bb0c262adfbf9b839394f3b6cda4dee05
AutisticCrumbs <ecrump@bsu.edu>
2023-10-01 16:14:25 -0400

    Add multiple different tiles that play different sound effects These sound effects play when the playable character comes into contact with them

--- sound_triggers/double_beep/double_beep.gd
+++ sound_triggers/double_beep/double_beep.gd
@@ -2,10 +2,9 @@ extends Area2D
 
 func _on_body_entered(_body):
 	var audio_stream_player := AudioStreamPlayer.new()
-	audio_stream_player.stream = load("res://sound_triggers/end_area/end_beep.wav")
+	audio_stream_player.stream = load("res://sound_triggers/double_beep/double_beep.wav")
 	get_parent().add_child(audio_stream_player)
 	audio_stream_player.play()
 	audio_stream_player.finished.connect(func():
 		audio_stream_player.queue_free()
-		get_tree().change_scene_to_file("res://end_screen/end_screen.tscn")
 	)
